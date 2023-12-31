fd465215f43bc371facf601475ed3a9b70bd8517
test <test200629@gmail.com>
2023-12-31 17:27:29 +0800

    231231_1726

--- src/main/kotlin/CsvFileWriter.kt
+++ src/main/kotlin/CsvFileWriter.kt
@@ -18,23 +18,32 @@ class CsvFileWriter(
             bw = BufferedWriter(FileWriter("$tabName.csv"))
 
             bw.write("Not in $dbName:\n")
+
             for (i in 0 ..< notInDBA.size) {
                 bw.write(notInDBA[i][0].toString())
+
                 for (j in 1..< notInDBA[i].size) bw.write(", ${notInDBA[i][j].toString()}")
+
                 bw.newLine()
             }
 
             bw.write("\nAdd in $dbName:\n")
+
             for (i in 0 ..< addInDBA.size) {
                 bw.write(addInDBA[i][0].toString())
+
                 for (j in 1..< addInDBA[i].size) bw.write(", ${addInDBA[i][j].toString()}")
+
                 bw.newLine()
             }
 
             bw.write("\nNot equal in $dbName:\n")
+
             for (i in 0 ..< xorInDBA.size) {
                 bw.write(xorInDBA[i][0].toString())
+
                 for (j in 1..< xorInDBA[i].size) bw.write(", ${xorInDBA[i][j].toString()}")
+
                 bw.newLine()
             }
         } catch (ioe: IOException) { error = true } finally {
